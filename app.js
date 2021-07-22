7128de4a45b4ea437cfbc0f886e26db821183acf
Paul Jeon <pauljeonn@gmail.com>
2021-07-22 23:21:19 +0900

    add validation and custom alert

--- app.js
+++ app.js
@@ -55,6 +55,17 @@ class UI {
 			el.parentElement.parentElement.remove();
 		}
 	}
+
+	static showAlert(message, className) {
+		const div = document.createElement('div');
+		div.className = `alert alert-${className}`;
+		div.appendChild(document.createTextNode(message));
+		const container = document.querySelector('.container');
+		const form = document.querySelector('#book-form');
+		container.insertBefore(div, form);
+		// vanish in 3 seconds
+		setTimeout(() => document.querySelector('.alert').remove(), 3000);
+	}
 }
 
 // Store Class: Handles Storage
@@ -72,14 +83,19 @@ document.querySelector('#book-form').addEventListener('submit', (e) => {
 	const author = document.querySelector('#author').value;
 	const isbn = document.querySelector('#isbn').value;
 
-	// Instantiate Book
-	const book = new Book(title, author, isbn);
+	// Validate
+	if (title === '' || author === '' || isbn === '') {
+		UI.showAlert('Please fill in all fields!', 'danger');
+	} else {
+		// Instantiate Book
+		const book = new Book(title, author, isbn);
 
-	// Add Book to UI
-	UI.addBookToList(book);
+		// Add Book to UI
+		UI.addBookToList(book);
 
-	// Clear fields
-	UI.clearFields();
+		// Clear fields
+		UI.clearFields();
+	}
 });
 
 // Event: Remove a Book
