fef7b8c339ddf495032200e508492ea18df3a320
Paul Jeon <pauljeonn@gmail.com>
2021-07-23 02:42:39 +0900

    handle storage

--- app.js
+++ app.js
@@ -9,20 +9,7 @@ class Book {
 // UI Class: Handle UI Tasks
 class UI {
 	static displayBooks() {
-		const StoredBooks = [
-			{
-				title: 'Book One',
-				author: 'John Doe',
-				isbn: '3434434',
-			},
-			{
-				title: 'Book Two',
-				author: 'Jane Doe',
-				isbn: '45545',
-			},
-		];
-
-		const books = StoredBooks;
+		const books = Store.getBooks();
 
 		books.forEach((book) => UI.addBookToList(book));
 	}
@@ -69,6 +56,36 @@ class UI {
 }
 
 // Store Class: Handles Storage
+class Store {
+	static getBooks() {
+		let books;
+		if (localStorage.getItem('books') === null) {
+			books = [];
+		} else {
+			books = JSON.parse(localStorage.getItem('books'));
+		}
+
+		return books;
+	}
+
+	static addBook(book) {
+		const books = Store.getBooks();
+		books.push(book);
+		localStorage.setItem('books', JSON.stringify(books));
+	}
+
+	static removeBook(isbn) {
+		const books = Store.getBooks();
+
+		books.forEach((book, index) => {
+			if (book.isbn === isbn) {
+				books.splice(index, 1);
+			}
+		});
+
+		localStorage.setItem('books', JSON.stringify(books));
+	}
+}
 
 // Event: Display Books
 document.addEventListener('DOMContentLoaded', UI.displayBooks);
@@ -93,6 +110,9 @@ document.querySelector('#book-form').addEventListener('submit', (e) => {
 		// Add Book to UI
 		UI.addBookToList(book);
 
+		// Add Book to Store
+		Store.addBook(book);
+
 		// Show success message
 		UI.showAlert('Book Added Successfully!', 'success');
 
@@ -106,7 +126,11 @@ document.querySelector('#book-list').addEventListener('click', (e) => {
 	// Console log the target and see what it is made of
 	console.log(e.target);
 
+	// Remove book from UI
 	UI.deleteBook(e.target);
+
+	// Remove book from store
+	Store.removeBook(e.target.parentElement.previousElementSibling.textContent);
 
 	// Show success message
 	UI.showAlert('Book removed', 'success');
